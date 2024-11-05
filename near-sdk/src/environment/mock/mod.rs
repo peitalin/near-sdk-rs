52abdc6a4bbe1bf547e325c54925873f77f3eaf3
Peita <n6378056@gmail.com>
2024-11-06 03:21:02 +0900

    add Rc to mocked blockchain

--- near-sdk/src/environment/mock/mod.rs
+++ near-sdk/src/environment/mock/mod.rs
@@ -7,12 +7,13 @@ pub use mocked_blockchain::test_vm_config;
 pub use self::mocked_blockchain::MockedBlockchain;
 pub use self::receipt::{MockAction, Receipt};
 use core::cell::RefCell;
+use std::rc::Rc;
 
 thread_local! {
     /// Low-level blockchain interface wrapped by the environment. Prefer using `env::*` and
     /// `testing_env` for interacting with the real and fake blockchains.
-    static BLOCKCHAIN_INTERFACE: RefCell<MockedBlockchain>
-         = RefCell::new(MockedBlockchain::default());
+    static BLOCKCHAIN_INTERFACE: Rc<RefCell<MockedBlockchain>>
+         = Rc::new(RefCell::new(MockedBlockchain::default()));
 }
 
 /// Perform function on a mutable reference to the [`MockedBlockchain`]. This can only be used
